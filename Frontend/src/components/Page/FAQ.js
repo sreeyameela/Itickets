bdc1998f75b6e7eb4a615c7e1eb71dd5ec344d23
sailakkineni <lsaishankar53@gmail.com>
2023-11-22 00:42:48 -0500

    changes in FAQ

--- Frontend/src/components/Page/FAQ.js
+++ Frontend/src/components/Page/FAQ.js
@@ -1,26 +1,24 @@
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./Styles.css";
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
-import { Col,Row } from 'react-bootstrap';
+import { Col, Row } from 'react-bootstrap';
 
 function CreateFAQ() {
   const [show, setShow] = useState(false);
-  const handleClose = () => setShow(false);
   const [image, setImage] = useState("");
-  const handleShow = () => {
-    console.log('handleShow called');
-    setShow(true);
-  };
-const  handleChange = (e) => {
+  const [selectedTopic, setSelectedTopic] = useState(null);
+  const [faqs, setFaqs] = useState([]);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  const handleChange = (e) => {
     setImage(e.target.files[0]);
-};
+  };
 
-// faq page 
-
-const FAQ = ({ faq, index, toggleFAQ }) => {
+  const FAQ = ({ faq, index, toggleFAQ }) => {
     return (
       <div
         className={"faq " + (faq.open ? "open" : "")}
@@ -32,42 +30,80 @@ const FAQ = ({ faq, index, toggleFAQ }) => {
       </div>
     );
   };
-  const [faqs, setFaqs] = useState([
-    {
-      question: "How many programmers does it take to screw a lightbulb?",
-      answer:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pharetra lorem eu dolor rhoncus, at scelerisque ligula gravida. Sed porta id mi sit amet convallis. Etiam iaculis massa sit amet lacus blandit sodales. Nulla ultrices velit a diam placerat congue. Pellentesque iaculis, ipsum quis eleifend dapibus, est dui eleifend ante, quis fermentum mi ligula quis nisl. Ut et ex dui. Integer id venenatis quam.",
-      open: true
-    },
-    {
-      question: "Who is the most awesome person?",
-      answer: "You! The viewer!",
-      open: false
-    },
-    {
-      question:
-        "How many questions does it take to makes a successful FAQ Page?",
-      answer: "This many!",
-      open: false
-    },
-    {
-        question: "How many programmers does it take to screw a lightbulb?",
-        answer:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pharetra lorem eu dolor rhoncus, at scelerisque ligula gravida. Sed porta id mi sit amet convallis. Etiam iaculis massa sit amet lacus blandit sodales. Nulla ultrices velit a diam placerat congue. Pellentesque iaculis, ipsum quis eleifend dapibus, est dui eleifend ante, quis fermentum mi ligula quis nisl. Ut et ex dui. Integer id venenatis quam.",
-        open: true
-      },
-      {
-        question: "Who is the most awesome person?",
-        answer: "You! The viewer!",
-        open: false
-      },
-      {
-        question:
-          "How many questions does it take to makes a successful FAQ Page?",
-        answer: "This many!",
-        open: false
-      }
-  ]);
+
+  useEffect(() => {
+    const fetchFAQs = async () => {
+      // Replace with your actual logic for fetching FAQs
+      // For example: const response = await fetch('your_api_endpoint');
+      // const data = await response.json();
+      // return data;
+      return [
+        {
+            topic: "Request for I20, Travel I20",
+            question: "How to travel get I20?",
+            answer: (
+              <>
+              <p>
+                Click <a href="https://accessisss.albany.edu/istart/controllers/start/StartEngine.cfm">here</a> to navigate to the AccessISSS online system. If you are logging in from off campus you must be logged in through the University's VPN. Login with your UAlbany NetID (ex: AA123456) and password.
+                </p >
+                <p>
+                Select the Request for Updated or Travel Signature I-20/DS-2019 from the EForm menu.
+                </p >
+                <p>
+                upload supporting documents. If you need help, please review the File Upload Instructions webpage.
+                </p>
+                All updates will be communicated over email. Please allow up to 5-7 business days for processing.
+              </>
+            ),
+            open: true,
+          },
+        {
+          topic: "Reduced Course Load Authorization (RCL)",
+          question: "How to apply RCL?",
+          answer:(
+            <>
+              <p>
+                Full-time enrollment at the University at Albany is 12 credits for
+                undergraduate students and 9 credits for graduate students...
+                (rest of the existing document)
+              </p>
+              <p>
+                After reviewing the above information, you may submit a Reduced Course
+                Load (RCL) Request EForm:
+              </p>
+              <ol>
+                <li>
+                  <a href="https://accessisss.albany.edu/istart/controllers/start/StartEngine.cfm">
+                    Click here
+                  </a>{" "}
+                  
+                </li>
+               
+              </ol>
+              <p>
+                All updates related to your EForm will be communicated over email.
+                Please allow up to 5-7 business days for processing.
+              </p>
+            </>
+          ),
+          open: false,
+        },
+        {
+          topic: "JavaScript",
+          question: "What is JavaScript?",
+          answer: "JavaScript is a programming language...",
+          open: false,
+        },
+      ];
+    };
+
+    const fetchData = async () => {
+      const fetchedFAQs = await fetchFAQs();
+      setFaqs(fetchedFAQs);
+    };
+
+    fetchData();
+  }, []);
 
   const toggleFAQ = (index) => {
     setFaqs(
@@ -83,65 +119,40 @@ const FAQ = ({ faq, index, toggleFAQ }) => {
     );
   };
 
-
   return (
-    
-    
-    <div className='container' style={{marginTop:"80px"}}>
-    <Button variant="primary" onClick={handleShow}>
-      Create FAQ
-    </Button>
-
-    <Modal show={show} onHide={handleClose} backdrop="static" keyboard={false}>
-      <Modal.Header closeButton>
-        <Modal.Title>Add Query</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <Form>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-            <Form.Label>Query</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Enter the Query"
-              autoFocus
-            />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-            <Form.Label>Answer</Form.Label>
-            <Form.Control as="textarea" rows={3} />
-          </Form.Group>
-          <Form.Group>
-            <Form.Label>Upload Image</Form.Label>
-            <Form.Control type="file" onChange={handleChange} />
-          </Form.Group>
-        </Form>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant="secondary" onClick={handleClose}>
-          Close
-        </Button>
-        <Button variant="primary" onClick={handleClose}>
-          Save Changes
-        </Button>
-      </Modal.Footer>
-    </Modal>
-      
-    <Row>
-      <Col xs={3}></Col>
-      <Col xs={9} md={9} lg={9}>
-        <div className="faq-list">
-          <div className="faqs">
-            {faqs.map((faq, index) => (
-              <FAQ faq={faq} index={index} key={index} toggleFAQ={toggleFAQ} />
-            ))}
+   <div className="faq-container" style={{ margin: "80px 0 0 80px" }}>
+      <Row>
+        <Col xs={12} md={8} lg={9}>
+          {/* FAQ Section */}
+          <div className="faq-list">
+            <div className="faqs">
+              {faqs.map((faq, index) => (
+                (selectedTopic === null || faq.topic === selectedTopic) && (
+                  <FAQ faq={faq} index={index} key={index} toggleFAQ={toggleFAQ} />
+                )
+              ))}
+            </div>
           </div>
-        </div>
-      </Col>
-    </Row>
-  </div>
-    
-
-
+        </Col>
+        <Col xs={12} md={4} lg={3} style={{ marginTop: "80px" }}>
+          {/* Topics Section */}
+          <div className="topics">
+            <h2>Topics</h2>
+            <ul>
+              {[...new Set(faqs.map((faq) => faq.topic))].map((topic, index) => (
+                <li
+                  key={index}
+                  onClick={() => setSelectedTopic(topic)}
+                  style={{ cursor: "pointer" }}
+                >
+                  {topic}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </Col>
+      </Row>
+    </div>
   );
 }
 
